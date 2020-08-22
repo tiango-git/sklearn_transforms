3d632df8b326a7386560e1589f20f5e5e9224788
tiango-git <54322464+tiango-git@users.noreply.github.com>
2020-08-22 17:59:33 -0400

    Update setup.py
    Cambiar los datos de contacto

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/tiango-git/sklearn_transforms/',
+      author='Cristian Gonzalez',
+      author_email='tiango@hotmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
